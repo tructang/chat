5d4eb77fb5b4f48b434c0b31b3b1c0e463675ad0
Truc.Tang <trucgolden@gmail.com>
2024-09-10 09:12:33 +0700

    init simple chat app

---
+++ src/test/java/qualgo/assessment/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package qualgo.assessment.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
